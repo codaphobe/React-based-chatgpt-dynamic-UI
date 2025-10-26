a53d11f53d5611476445a7c7e39a1c79b0b404b4
codephobe <kutakanakerisanjay@gmail.com>
2025-10-26 16:55:59 +0530

    Changed main.jsx to render the root

---
+++ src/ThreadViewBtn.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ThreadViewBtn() {
+  
+  function handleClick() {
+    console.log("Thread View btn clicked")
+  }
+  
+  return (
+    <button className='btn btn-ghost bg-gray-700 w-34 h-8 px-2 py-1' onClick={handleClick()}>Thread View</button>
+  )
+}
+
+export default ThreadViewBtn;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,32 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ThreadViewBtn from './ThreadViewBtn.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const container = document.querySelector("#thread");
+let timer;
+
+function addRoot(header){
+  const headerActions = header.children[2].children.namedItem("conversation-header-actions");
+  if(headerActions && !header.getElementById("threadView-root")){
+    const rootContainer = document.createElement("div");
+    rootContainer.setAttribute("id","threadView-root");
+    headerActions.prepend(rootContainer);
+    
+    const root = createRoot(rootContainer);
+    root.render(
+      <ThreadViewBtn/>
+    )
+  };
+}
+
+const observer = new MutationObserver(() => {
+   const header = container.querySelector("#page-header");
+   clearTimeout(timer);
+   const db = setTimeout(() =>{
+         addRoot(header);
+   }, 2000)
+});
+
+observer.observe(container,{childList:true,subtree:true});
+

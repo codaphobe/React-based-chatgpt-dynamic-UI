0fe5ec16aba20d338eb5d8217a9b1d658ecc1b23
codephobe <kutakanakerisanjay@gmail.com>
2025-11-04 22:33:44 +0530

    All messages/chats are stored into Objects using Mutation Observer

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import ThreadViewBtn from './ThreadViewBtn.jsx';
+import ThreadUtils from './utils/ThreadUtils.js';
 
-const container = document.querySelector("#thread");
-let timer;
 
 function addRoot(header){
-  const headerActions = header.children[2].children.namedItem("conversation-header-actions");
+  const headerActions = header.querySelector("#conversation-header-actions");
   if(headerActions && !header.querySelector("#threadView-root")){
     const rootContainer = document.createElement("div");
     rootContainer.setAttribute("id","threadView-root");
@@ -19,13 +18,44 @@ function addRoot(header){
   };
 }
 
+const container = document.querySelector("#thread");
+let timer;
+const threadUtils = new ThreadUtils();
 const observer = new MutationObserver(() => {
-   const header = container.querySelector("#page-header");
-   clearTimeout(timer);
-   const db = setTimeout(() =>{
-         addRoot(header);
-   }, 2000)
+  clearTimeout(timer);
+  timer = setTimeout(() =>{
+      const header = container.querySelector("#page-header");
+      const chatContainer = container.querySelector("div:has(> article)");
+      if (header && chatContainer){
+        addRoot(header);
+        threadUtils.initializeThreads(chatContainer);
+        const articlesCount = container.querySelectorAll("article").length;
+        if (articlesCount === threadUtils.allChats.size) {
+          // All articles have been processed, disconnect observer
+          observer.disconnect();
+          
+          //start new chats observer
+          newChatsObserverStart(chatContainer);
+        }
+      }
+   }, 1000);
 });
 
 observer.observe(container,{childList:true,subtree:true});
 
+function newChatsObserverStart(chatContainer){
+    let timer;
+    const newChatObserver = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) =>{
+          if(mutation.type === "childList"){
+            mutation.addedNodes.forEach((node) =>{
+              if (node.tagName === "ARTICLE"){
+                console.log("New article added, rebuilding threads...");
+                console.log(node);
+              }
+            });
+          }
+        });
+    });
+    newChatObserver.observe(chatContainer,{childList:true,subtree:true});
+}
---
+++ src/utils/ThreadUtils.js
@@ -0,0 +1,68 @@
+export default class ThreadUtils {
+    constructor() {
+        this.allChats = new Map();
+        this.allThreads = new Map();
+    }
+
+    initializeThreads(chatContainer) {
+        
+        // Collect all chats
+        this.collectAllChats(chatContainer);
+        
+        // Build threads from chats
+        this.buildThreads();
+    }
+    
+    collectAllChats(chatContainer) {
+        const articles = chatContainer.querySelectorAll("article");
+        
+        if (articles.length === 0) {
+            console.log("No articles found in chat container.");
+            return;
+        }
+        
+        if (articles.length === this.allChats.size) {
+            console.log("All articles have already been collected.");
+            return;
+        }
+        
+        //Collect all chats
+        articles.forEach((article) => {
+            this.allChats.set(article.attributes.getNamedItem("data-testid").value,{
+            "element":article,
+            "turn":article.attributes.getNamedItem('data-turn').value
+            });
+        });
+        console.log(this.allChats);
+    }
+        
+    
+    buildThreads(){
+        
+        if(this.allChats.size===0){
+            console.log("No chats available to build threads.");
+            return;
+        }
+
+        let threadCount = 0;
+        for(let i=0;i<this.allChats.size;i++){
+            const user = this.allChats.get(`conversation-turn-${i+1}`);
+            const assistant = this.allChats.get(`conversation-turn-${i+2}`);
+
+            if (user["turn"] === "user" && assistant["turn"]==="assistant"){
+                this.allThreads.set(`thread-id-${threadCount}`,{
+                    user : user["element"],
+                    assistant : assistant["element"],
+                    collapsed :true
+                })
+                threadCount++;
+                i++;
+            }   
+        }
+        console.log(this.allThreads);
+    }
+    
+    addNewChats(articels){
+        
+    }
+}
